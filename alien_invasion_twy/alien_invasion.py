826ae488498380a1ba6c53c16dc8ec1864e3c733
YosparNa <2730094242@qq.com>
2024-11-15 10:01:50 +0800

    外星人入侵游戏开发

--- alien_invasion_twy/alien_invasion.py
+++ alien_invasion_twy/alien_invasion.py
@@ -1,49 +1,42 @@
 import sys
 import pygame
-
 from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
 
+def run_game():
+    # 初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
 
-class AlienInvasion:
-    """管理游戏资源和行为的类"""
-    def __init__(self):
-        """初始化游戏，并创建游戏资源"""
-        pygame.init()
-        self.clock = pygame.time.Clock()
-        #snip#
-        
-        pygame.display.set_caption("Alien Invasion")
-        # 设置背景色
-        self.bg_color = (230, 230, 230)
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
 
+    # 创建一个用于存储游戏统计信息的实例,并创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
-        self.settings = Settings()
-        
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
-    def run_game(self):
-        """开始游戏的主循环"""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # 每次循环时都重绘屏幕
-        self.screen.fill(self.settings.bg_color)
-            # 让最近绘制的屏幕可见
-        pygame.display.flip()
-        self.clock.tick(60)
-        
-        while True:
-            pygame.display.flip()
-            self.clock.tick(60)
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
-if __name__ == '__main__':
-    # 创建游戏实例并运行游戏
-    ai = AlienInvasion()
-    ai.run_game()
+    # 创建一艘飞船、一个子弹编组和一个外星人编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 开始游戏的主循环
+    while True:
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+run_game()
